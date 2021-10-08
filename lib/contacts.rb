92e10665fe5937e1faf7a688c60973c036f489b6
greatmedivack <putinihin@medods.ru>
2021-10-08 14:03:34 +0500

    first commit

---
+++ lib/contacts.rb
@@ -0,0 +1,54 @@
+VALID_PHONE_NUMBER_REGEX = /\+?(\d\-?)+( доб\. )?/
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+def create_contacts_list(source_file)
+  file = File.new('./public/res.csv', 'w')
+  result = CSV.open(file, 'w', col_sep: ';', headers: true)
+
+  tmp_row = []
+
+  CSV.readlines(source_file).each do |row|
+      contact_name = row[2].to_s.strip
+      position = row[57].to_s.strip
+      #Emails
+      emails = []
+    	emails << filter_emails(row[24])
+    	emails << filter_emails(row[25])
+    	emails << filter_emails(row[26])
+      #Phones
+      phones = []
+      phones << filter_phones(row[18])
+      phones << filter_phones(row[19])
+      phones << filter_phones(row[20])
+      phones << filter_phones(row[21])
+      phones << filter_phones(row[22])
+      phones << filter_phones(row[23])
+
+      emails.flatten!
+    	phones.flatten!
+
+      array = 
+            if emails.size >= phones.size
+              emails.each_with_index.map {|x, i| [contact_name.to_s,row[57].to_s,x.to_s,phones[i].to_s]}
+            else
+              phones.each_with_index.map {|x, i| [position.to_s,row[57].to_s,emails[i].to_s,x.to_s]}
+            end
+
+      array.each do |array|
+        result << array.flatten unless (array[2].empty? && array[3].empty?) || (array[0].empty? && array[1].empty?)
+      end  
+
+    end
+end
+
+def filter_emails(string)
+  string = "" if string.nil?
+  tmp = string.split(',').map {|email| email.strip}
+  tmp.select { |line| line =~ VALID_EMAIL_REGEX }
+end
+
+def filter_phones(string)
+	string = "" if string.nil?
+	tmp = string.split(',').map {|phone| phone.gsub(/\'/,'')}
+	tmp.select { |line| line =~ VALID_PHONE_NUMBER_REGEX }
+end
